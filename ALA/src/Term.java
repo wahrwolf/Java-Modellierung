763ccb3f147e1ad56c15721d9215e28258d22025
Vincent <4dahmen@informatik.uni-hamburg.de>
2015-04-07 23:37:07 +0200

    ALA init

---
+++ ALA/src/Term.java
@@ -0,0 +1,45 @@
+
+public class Term {
+	
+	private double _koeffizient;
+	private String _variable;
+
+	
+	public Term(double _koeffizient, String _variable) {
+		this._koeffizient = _koeffizient;
+		this._variable = _variable;
+	}
+
+	public double getKoeffizient()
+	{
+		return _koeffizient;
+	}
+	
+	public void setKoeffizient(double value)
+	{
+		_koeffizient = value;
+	}
+	
+	public String getVariable()
+	{
+		return _variable;
+	}
+	
+	public String toString()
+	{
+		return _koeffizient + _variable;
+	}
+	
+	public void multiplizieren(double faktor)
+	{
+		_koeffizient *= faktor;
+	}
+	
+	public void addiere(Term summand)
+	{
+		if ( _variable.equals( summand.getVariable() ) )
+		{
+			_koeffizient += summand.getKoeffizient();
+		}
+	}
+}
