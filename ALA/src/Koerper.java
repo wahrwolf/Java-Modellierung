87466b767ee5564624a2c577a5ce6b6f5c2305fc
Vincent <4dahmen@informatik.uni-hamburg.de>
2015-04-19 19:08:36 +0200

    init and basic form of Element and Koerper (really basic !!!!!!)

---
+++ ALA/src/Koerper.java
@@ -0,0 +1,109 @@
+import java.util.Collection;
+import java.util.Iterator;
+
+
+public class Koerper implements Collection<Object>{
+	private Element _neutralesElementPlus;
+	private Element _neutralesElementMal;
+	private boolean _istFixiert;
+	
+	public Element gibInversesElementPlus(Element element)
+	{
+		return null;
+	}
+	
+	public Element gibInversesElementMal(Element element)
+	{
+		return null;
+	}
+	
+	public Element plus(Element summandA, Element summandB)
+	{
+		return null;
+	}
+	
+	public Element mal(Element faktorA, Element faktorB)
+	{
+		return null;
+	}
+	
+	
+
+	@Override
+	public boolean add(Object e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addAll(Collection c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterator iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeAll(Collection c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] toArray(Object[] a) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
